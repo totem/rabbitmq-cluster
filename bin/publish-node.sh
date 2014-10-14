23fcbfccd13ee7a12afe6c7e0c8004da9ac84cda
sukrit007 <sukrit007@gmail.com>
2014-10-14 15:40:38 -0700

    Use HOST_IP instead of NODE_IP

--- bin/publish-node.sh
+++ bin/publish-node.sh
@@ -7,11 +7,11 @@ PUBLISH_NODE_TTL=${PUBLISH_NODE_TTL:-120}
 PUBLISH_NODE_POLL=${PUBLISH_NODE_POLL:-60s}
 
 NODE=$(cat /var/lib/rabbitmq/nodename)
-NODE_IP=${NODE_IP:-$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')}
+HOST_IP=${HOST_IP:-$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')}
 
 while supervisorctl status rabbitmq-server | grep 'RUNNING'
 do
-    echo "Publishing $ETCD_RABBITMQ_BASE/rabbitmq/nodes/${NODE} ${NODE_IP} with ttl ${PUBLISH_NODE_TTL}"
-	${ETCDCTL} set --ttl ${PUBLISH_NODE_TTL} $ETCD_RABBITMQ_BASE/rabbitmq/nodes/${NODE} ${NODE_IP}
+    echo "Publishing $ETCD_RABBITMQ_BASE/rabbitmq/nodes/${NODE} ${HOST_IP} with ttl ${PUBLISH_NODE_TTL}"
+	${ETCDCTL} set --ttl ${PUBLISH_NODE_TTL} $ETCD_RABBITMQ_BASE/rabbitmq/nodes/${NODE} ${HOST_IP}
 	sleep ${PUBLISH_NODE_POLL}
 done
